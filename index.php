23e6649de3cf2c7b39bbbdc5267f9eb44b6a0845
slowhand <slowhand@192.168.1.36>
2018-02-25 14:10:33 +0100

    cuarto commit

--- index.php
+++ index.php
@@ -1,18 +1,123 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Documentation & Examples - comboAssociate - jQuery Plugin">
+        <meta name="author" content="comboAssociate - jQuery Plugin">
+        <title>Documentation & Examples - comboAssociate - jQuery Plugin</title>
+        <!-- Bootstrap core CSS -->
+        <link href="resources/bootstrap-4.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="resources/bootstrap-4.0.0/docs/4.0/examples/album/album.css" rel="stylesheet">
     </head>
     <body>
-        <?php
-        // put your code here
-        echo 'si'; 
-        ?>
+
+        <header>
+          <div class="collapse bg-dark" id="navbarHeader">
+                <div class="container">
+                  <div class="row">
+                        <div class="col-sm-8 col-md-7 py-4">
+                          <h4 class="text-white">About</h4>
+                          <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+                        </div>
+                        <div class="col-sm-4 offset-md-1 py-4">
+                          <h4 class="text-white">Contact</h4>
+                          <ul class="list-unstyled">
+                                <li><a href="#" class="text-white">Email me</a></li>
+                          </ul>
+                        </div>
+                  </div>
+                </div>
+          </div>
+          <div class="navbar navbar-dark bg-dark box-shadow">
+                <div class="container d-flex justify-content-between">
+                  <a href="#" class="navbar-brand d-flex align-items-center">
+                        <strong>comboAssociate - jQuery Plugin</strong>
+                  </a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                  </button>
+                </div>
+          </div>
+        </header>
+
+        <main role="main">
+
+          <section class="jumbotron text-center">
+                <div class="container">
+                  <h1 class="jumbotron-heading">comboAssociate - jQuery Plugin</h1>
+                  <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+                </div>
+          </section>
+
+          <div class="album py-5 bg-light">
+                <div class="container">
+
+                  <div class="row">
+                        <div class="col-md-4">
+                          <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Basic Example 1" alt="Basic Example 1">
+                                <div class="card-body">
+                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                  <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                          <button type="button" class="btn btn-sm btn-outline-secondary" onclick="window.location='examples/basic-example-1.html';" >View</button>
+                                        </div>
+                                  </div>
+                                </div>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Basic Example 2" alt="Basic Example 2">
+                                <div class="card-body">
+                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                  <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                        </div>
+                                  </div>
+                                </div>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Basic Example 3" alt="Basic Example 3">
+                                <div class="card-body">
+                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                  <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                        </div>
+                                  </div>
+                                </div>
+                          </div>
+                        </div>
+                  </div>
+                </div>
+          </div>
+
+        </main>
+
+        <footer class="text-muted">
+          <div class="container">
+                <p class="float-right">
+                  <a href="#">Back to top</a>
+                </p>
+                <p>comboAssociate - jQuery Plugin</p>
+                <p>The plugin needs jQuery to work. Visit jQuery website - <a target="_blank" href="https://jquery.com/">jQuery</a></p>
+                <p>The plugin doesnt needs bootstrap to work. We only use Bootstrap (version 4.0.0) for docs and examples - <a target="_blank" href="https://getbootstrap.com/">Visit Bootstrap website</a></p>
+          </div>
+        </footer>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="resources/bootstrap-4.0.0/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+        <script src="resources/bootstrap-4.0.0/assets/js/vendor/popper.min.js"></script>
+        <script src="resources/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
+        <script src="resources/bootstrap-4.0.0/assets/js/vendor/holder.min.js"></script>
     </body>
 </html>
--- resources/js/jquery-comboAssociate-v1-0.js
+++ resources/js/jquery-comboAssociate-v1-0.js
@@ -3,96 +3,96 @@
  *
  */
 (function ($) {
-	 $.fn.comboAssociate = function(settings) {
-		//this - the element who calls comboAssociate() //selectCombo1
-		
-		if( (typeof settings.debug === 'undefined') || (settings.debug == false) ){var comboAssociateDebug = true;} 
-		if(typeof settings === 'undefined'){alert('Error: comboAssociate initializing error: no settings');return false;}
-		if(typeof settings.associatedCombos === 'undefined'){alert('Error: comboAssociate initializing error: no associatedCombos option on settings');return false;}
-		if(settings.associatedCombos.length == 0){alert('Error: comboAssociate initializing error: settings.associatedCombos is empty');return false;}
-		
-		var urlIsPresent = 0;
-		for (var key in settings.associatedCombos) {
-			var obj = settings.associatedCombos[key];
-			for (var prop in obj) {
-				if(prop==='url'){
-					//alert('hay url');
-					urlIsPresent++;
-				}
-				//alert(key +'->'+ prop + " = " + obj[prop]);
-			}
-		}
-		alert(urlIsPresent + '-' + settings.associatedCombos.length);
-		if( urlIsPresent < settings.associatedCombos.length ){alert('Error: comboAssociate initializing error: no url option on any position of settings.associatedCombos');return false;}
-		
-		
-		/* Default values to use in this plugin - don't change */
-		var options = $.extend({
-			//debug: false, /*no es necesrio de momento creo yo...*/	
-			chooseOption: false,
-			chooseOptionValue: '0',
-			beforeFunction: function (object) { 
-				console.log('default beforeFunction selectCombo2');
-			},
-			afterFunction: function (object) {
-				console.log('default afterFunction selectCombo2');
-			}					
-        }, settings);
-		
-		
-		if(comboAssociateDebug){console.log(options);}
-		
-		
-		
-		this.on('change', function() {
-			var selectedOption = $(this).val();
-			alert(selectedOption);
-		});
-		
-		return false;
-		/* //////////////////////////////comboAssociate Plugin//////////////////////////////////////////////////////// */
-		
-		
-		
-		
-		
-		
-		$.fn.comboAssociateAjaxCall = function(customAjaxOptions) {
+        $.fn.comboAssociate = function(settings) {
+            //this - the element who calls comboAssociate() //selectCombo1
 
-			var defaultOptions = { type: "POST", data: {}, dataType: "json", success: function(response){alert(response.message);}, error: function(xhr, ajaxOptions, thrownError){alert(thrownError);} };
-			
-			customAjaxOptions = $.extend(true,defaultOptions, customAjaxOptions);
-			
-			if(!customAjaxOptions.url){console.log('*****************');console.log('ERROR: FALTA URL PARA LLAMADA AJAX EN LAS OPCIONES');console.log('*****************');return false;}
-			
-			
-			/* WARNING:......LARAVEL???*/ 
-			if(!customAjaxOptions.data._token){ customAjaxOptions.data._token = "{{csrf_token()}}";}
-			
-			$.ajax(customAjaxOptions);	
-		};
-		
-		//GESTIONA LOS X SELECTORES RECIBIDOS
-		$.fn.gestionarSelectElemento = function(params) {
-			var selectElemento = $('select#selectElemento option:selected').val();
-		};
-		
-		/*function gestionarSelectElemento(){
-            //recogemos el id del elemento seleccionado (ejemplo: id de un Hito)
-            var selectElemento = $('select#selectElemento option:selected').val();
-            if(selectElemento == 0){
-                //deshabilitamos el boton de subida
-                $("#btnUploadFile").attr("disabled", true);
-                $('#contenedorInputFile').css('display', 'none');
-            }else{
-                //habilitamos el boton de subida
-                $("#btnUploadFile").attr("disabled", false);
-                $('#contenedorInputFile').css('display', 'block');
+            if( (typeof settings.debug === 'undefined') || (settings.debug == false) ){var comboAssociateDebug = true;} 
+            if(typeof settings === 'undefined'){alert('Error: comboAssociate initializing error: no settings');return false;}
+            if(typeof settings.associatedCombos === 'undefined'){alert('Error: comboAssociate initializing error: no associatedCombos option on settings');return false;}
+            if(settings.associatedCombos.length == 0){alert('Error: comboAssociate initializing error: settings.associatedCombos is empty');return false;}
+
+            var urlIsPresent = 0;
+            for (var key in settings.associatedCombos) {
+                    var obj = settings.associatedCombos[key];
+                    for (var prop in obj) {
+                            if(prop==='url'){
+                                    //alert('hay url');
+                                    urlIsPresent++;
+                            }
+                            //alert(key +'->'+ prop + " = " + obj[prop]);
+                    }
             }
-            return false;
-        }*/
+            alert(urlIsPresent + '-' + settings.associatedCombos.length);
+            if( urlIsPresent < settings.associatedCombos.length ){alert('Error: comboAssociate initializing error: no url option on any position of settings.associatedCombos');return false;}
 
-		/* FINALLY - We return the object - It's useful when you need to chaining other jQuery functions before initializing the comboAssociate jQuery Plugin - See the docs for more info*/
-		return this; 
-	};
+
+            /* Default values to use in this plugin - don't change */
+            var options = $.extend({
+                    //debug: false, /*no es necesrio de momento creo yo...*/	
+                    chooseOption: false,
+                    chooseOptionValue: '0',
+                    beforeFunction: function (object) { 
+                            console.log('default beforeFunction selectCombo2');
+                    },
+                    afterFunction: function (object) {
+                            console.log('default afterFunction selectCombo2');
+                    }					
+            }, settings);
+
+
+            if(comboAssociateDebug){console.log(options);}
+
+
+
+            this.on('change', function() {
+                    var selectedOption = $(this).val();
+                    alert(selectedOption);
+            });
+
+            return false;
+            /* //////////////////////////////comboAssociate Plugin//////////////////////////////////////////////////////// */
+
+
+
+
+
+
+            $.fn.comboAssociateAjaxCall = function(customAjaxOptions) {
+
+                    var defaultOptions = { type: "POST", data: {}, dataType: "json", success: function(response){alert(response.message);}, error: function(xhr, ajaxOptions, thrownError){alert(thrownError);} };
+
+                    customAjaxOptions = $.extend(true,defaultOptions, customAjaxOptions);
+
+                    if(!customAjaxOptions.url){console.log('*****************');console.log('ERROR: FALTA URL PARA LLAMADA AJAX EN LAS OPCIONES');console.log('*****************');return false;}
+
+
+                    /* WARNING:......LARAVEL???*/ 
+                    if(!customAjaxOptions.data._token){ customAjaxOptions.data._token = "{{csrf_token()}}";}
+
+                    $.ajax(customAjaxOptions);	
+            };
+
+            //GESTIONA LOS X SELECTORES RECIBIDOS
+            $.fn.gestionarSelectElemento = function(params) {
+                    var selectElemento = $('select#selectElemento option:selected').val();
+            };
+
+            /*function gestionarSelectElemento(){
+                //recogemos el id del elemento seleccionado (ejemplo: id de un Hito)
+                var selectElemento = $('select#selectElemento option:selected').val();
+                if(selectElemento == 0){
+                    //deshabilitamos el boton de subida
+                    $("#btnUploadFile").attr("disabled", true);
+                    $('#contenedorInputFile').css('display', 'none');
+                }else{
+                    //habilitamos el boton de subida
+                    $("#btnUploadFile").attr("disabled", false);
+                    $('#contenedorInputFile').css('display', 'block');
+                }
+                return false;
+            }*/
+
+            /* FINALLY - We return the object - It's useful when you need to chaining other jQuery functions before initializing the comboAssociate jQuery Plugin - See the docs for more info*/
+            return this; 
+       };
 }( jQuery ));
